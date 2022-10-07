1615f11f339b8bacd014db8351ad885bd43588ea
kate <katerina.savras@gmail.com>
2022-10-07 13:45:33 +0300

    add adaptive

--- static-templates/resources/scripts/shared/tab-slider.js
+++ static-templates/resources/scripts/shared/tab-slider.js
@@ -5,8 +5,9 @@ class TabSlider {
         this.tabItems = this.section.querySelectorAll('.tab');
         this.tabSlide = this.section.querySelectorAll('.tab-slide');
         this.navButtons = this.section.querySelectorAll('.tab-slider__btn');
-        this.prevButton = this.section.querySelectorAll('.tab-slider__btn_prev');
-        this.nextButton = this.section.querySelectorAll('.tab-slider__btn_next');
+        this.prevButton = this.section.querySelector('.prev');
+        this.nextButton = this.section.querySelector('.next');
+        this.colors = ['#BEED60', '#AEE856', '#9EE24B', '#94D841', '#89CD37', '#7FC22D', '#6BAD19', '#60A20F'];
         this.activeSlideClass = 'active';
         this.activeItemIndex = 0;
         this.bindAction = this.bindAction.bind(this);
@@ -14,7 +15,16 @@ class TabSlider {
         this.setActiveSlide = this.setActiveSlide.bind(this);
         this.changeSlide = this.changeSlide.bind(this);
         this.removeActiveClass = this.removeActiveClass.bind(this);
+        this.setColors = this.setColors.bind(this);
         this.init();
+    }
+
+    setColors(index) {
+        this.tabSlideItems = this.tabSlide[index].querySelectorAll('.tab-slide__item');
+        this.tabSlideItems.forEach((item, i) => {
+            item.style.setProperty('--main-color', this.colors[i]);
+            item.style.setProperty('--next-color', this.colors[i + 1]);
+        })
     }
 
     removeActiveClass(index) {
@@ -39,11 +49,35 @@ class TabSlider {
         this.tabSlide[index].classList.add(
             this.activeSlideClass
         );
+        this.setColors(index)
     }
 
-    changeSlide(index) {
+
+    changeSlide(index, btn) {
         this.removeActiveClass(this.activeItemIndex)
-        this.activeItemIndex = index;
+        if (index === null) {
+            btn === "next" ? this.activeItemIndex++ : this.activeItemIndex--;
+        } else {
+            this.activeItemIndex = index;
+        }
+        if (this.activeItemIndex === this.tabItems.length - 1) {
+            this.nextButton.classList.remove(
+                this.activeSlideClass
+            )
+        } else {
+            this.nextButton.classList.add(
+                this.activeSlideClass
+            )
+        }
+        if (this.activeItemIndex === 0) {
+            this.prevButton.classList.remove(
+                this.activeSlideClass
+            )
+        } else {
+            this.prevButton.classList.add(
+                this.activeSlideClass
+            )
+        }
         this.setActiveSlide(this.activeItemIndex)
     }
 
@@ -55,15 +89,35 @@ class TabSlider {
         });
     }
 
-    handleBtnClick(item, index) {
-        item.addEventListener("click", () => {
-            console.log("click")
+    handleBtnClick(item) {
+        item.addEventListener("click", (e) => {
+            if (e.target.classList.contains('next')) {
+                if (this.activeItemIndex === this.tabItems.length - 2) {
+                    this.changeSlide(null, "next")
+                    this.nextButton.classList.remove(
+                        this.activeSlideClass
+                    )
+                } else {
+                    this.changeSlide(null, "next")
+                }
+            } else if (e.target.classList.contains('prev')) {
+                if (this.activeItemIndex === 1) {
+                    this.changeSlide(null, "prev")
+                    this.prevButton.classList.remove(
+                        this.activeSlideClass
+                    )
+                } else {
+                    this.changeSlide(null, "prev")
+                }
+            }
         });
     }
 
 
     init() {
+        this.setColors(this.activeItemIndex)
         this.tabItems.forEach(this.bindAction);
+        this.tagItems.forEach(this.bindAction);
         this.navButtons.forEach(this.handleBtnClick);
     }
 }
