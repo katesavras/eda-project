ec90d49dd10a87b7a87d9bd41a64627ebdcb6f32
kate <katerina.savras@gmail.com>
2022-10-13 18:48:22 +0300

    add swiper

--- static-templates/resources/scripts/shared/img-slider.js
+++ static-templates/resources/scripts/shared/img-slider.js
@@ -1,47 +1,29 @@
-import { Swiper, Navigation, EffectFade, Pagination } from 'swiper';
+import {Swiper, Pagination, Thumbs} from 'swiper';
 
 class ImgSlider {
-    // constructor(section) {
-    //     this.section = section;
-    //     this.navItems = this.section.querySelectorAll('.img-slider__nav-item');
-    //     this.navImages = this.section.querySelectorAll('.img-slider__nav-image-wrap');
-    //     this.images = this.section.querySelectorAll('.img-slider__image-wrap');
-    //     this.activeClass = 'active';
-    //     this.activeItemIndex = 0;
-    //     this.bindAction = this.bindAction.bind(this);
-    //     this.setActiveSlide = this.setActiveSlide.bind(this);
-    //     this.changeSlide = this.changeSlide.bind(this);
-    //     this.removeActiveClass = this.removeActiveClass.bind(this);
-    //     this.init();
-    // }
-
     constructor() {
         this.init();
     }
 
     initSwiper(el) {
-        const slider = el.querySelector('.swiper');
-
-        new Swiper(slider, {
+        const slider = el.querySelector('.gallery-image');
+        const gallery = el.querySelector('.gallery-images');
+        const gallerySwiper = new Swiper(gallery, {
             modules: [Pagination],
-            pagination: {
-                el: '.swiper-pagination',
-                clickable: true,
-                type: 'bullets',
-                // el: '.swiper-pagination',
-                // clickable: true,
-                // renderBullet: function (index, className) {
-                //     return `<span class="img-slider__nav-item">${index}</span>`;
-                // },
-            },
+            spaceBetween: 10,
+            slidesPerView: 4,
+        });
+        const sliderSwiper = new Swiper(slider, {
+            modules: [Pagination, Thumbs],
             slidesPerView: 1,
-
-            // breakpoints: {
-            //     992: {
-            //         slidesPerView: 2.2,
-            //         spaceBetween: 20,
-            //     },
-            // },
+            pagination: {
+                el: ".swiper-pagination",
+                clickable: true,
+                type: "bullets",
+            },
+            thumbs: {
+                swiper: gallerySwiper,
+            },
         });
     }
 
